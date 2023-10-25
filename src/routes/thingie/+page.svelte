83e600e45dc60962ac82f005207efc21572d19f9
faun3 <antonioculcea@gmail.com>
2023-10-25 21:03:35 +0300

    add first route and first component to app

---
+++ src/routes/thingie/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let artists = [
+    {
+      name: "Fleetwood Mac",
+      albums: [
+        {
+          name: "Tango In The Night",
+          year: 1987,
+          tracks: [
+            { title: "Big Love", length: "3:37" },
+            { title: "Seven Wonders", length: "3:38" },
+            { title: "Everywhere", length: "3:43" },
+            { title: "Caroline", length: "3:50" },
+            { title: "Tango In The Night", length: "3:56" },
+            { title: "Mystified", length: "3:08" },
+          ],
+        },
+      ],
+    },
+  ];
+</script>
+
+<p>This is amazing!</p>
