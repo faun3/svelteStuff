c01b6dd9dbd1951177c6990015e2cd2e17020573
faun3 <antonioculcea@gmail.com>
2023-10-25 21:58:41 +0300

    use children in svelte wooo

---
+++ src/components/Button.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<button>
+  <slot>Placeholder</slot>
+</button>
+
+<style lang="scss">
+  button {
+    padding-block: 0.75em;
+    padding-inline: 1.5em;
+    border-radius: 10px;
+    background-color: teal;
+    color: white;
+    font-size: 1.15rem;
+    font-weight: bold;
+    border: none;
+    &:active {
+      transform: scale(0.95);
+    }
+    &:hover {
+      background-color: blue;
+    }
+  }
+</style>
---
+++ src/routes/morestuff/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Button from "../../components/Button.svelte";
+</script>
+
+<Button>
+  <span>This is just like passing children in React</span>
+  <div>But a whole lot easier!</div>
+  <button>It even works with weird stuff like this</button>
+  <input type="text" value="Or this" />
+</Button>
